2edc2de6723cf9cdd50807263859e5e676d927e9
Ji Wang <wj42421999@gmail.com>
2022-05-20 12:19:55 -0400

    Simplify routing

--- server/api.js
+++ server/api.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const apiRouter = express.Router();
 
-const minionsRouter = require('./routes/minions');
-const ideasRouter = require('./routes/ideas');
-const meetingsRouter = require('./routes/meetings');
-
-apiRouter.use('/minions', minionsRouter);
-apiRouter.use('/ideas', ideasRouter);
-apiRouter.use('/meetings', meetingsRouter);
+apiRouter.use("/minions", require("./routes/minions"));
+apiRouter.use("/ideas", require("./routes/ideas"));
+apiRouter.use("/meetings", require("./routes/meetings"));
 
 module.exports = apiRouter;

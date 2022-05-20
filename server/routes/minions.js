fe294af8665a2b0d7741b400d9c35785f00ff0ab
Ji Wang <wj42421999@gmail.com>
2022-05-20 14:49:30 -0400

    CURD minions

--- server/routes/minions.js
+++ server/routes/minions.js
@@ -1,9 +1,44 @@
 const express = require("express");
-const minionsRouter = express.Router();
+const router = express.Router();
 const db = require("../db");
 
-minionsRouter.get("", (req, res) => {
+router.param("minionId", (req, res, next, id) => {
+    const found = db.getFromDatabaseById("minions", id);
+    if (found) {
+        req.minionId = id;
+        next();
+    } else {
+        res.status(404).send();
+    }
+});
+
+router.get("", (req, res) => {
     res.send(db.getAllFromDatabase("minions"));
 });
 
-module.exports = minionsRouter;
+router.get("/:minionId", (req, res) => {
+    res.send(db.getFromDatabaseById("minions", req.minionId));
+});
+
+router.post("", (req, res) => {
+    const newItem = db.addToDatabase("minions", req.body);
+    if (newItem) {
+        res.status(201).send(newItem);
+    } else {
+        res.status(500).send();
+    }
+});
+
+router.put("/:minionId", (req, res) => {
+    res.send(db.updateInstanceInDatabase("minions", req.body));
+});
+
+router.delete("/:minionId", (req, res) => {
+    const deleted = db.deleteFromDatabasebyId("minions", req.minionId);
+    if (deleted) {
+        res.status(204).send();
+    }
+});
+
+
+module.exports = router;

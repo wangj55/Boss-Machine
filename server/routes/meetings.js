801dcf3ff79fe30daecb1048188eb4daa01cc157
Ji Wang <wj42421999@gmail.com>
2022-05-20 21:15:53 -0400

    CURD meetings

--- server/routes/meetings.js
+++ server/routes/meetings.js
@@ -1,9 +1,26 @@
 const express = require("express");
-const meetingsRouter = express.Router();
+const router = express.Router();
 const db = require("../db");
 
-meetingsRouter.get("", (req, res) => {
-    res.send(db.getAllFromDatabase("meetings"));
+const model = "meetings";
+
+router.get("", (req, res) => {
+    res.send(db.getAllFromDatabase(model));
 });
 
-module.exports = meetingsRouter;
+router.post("", (req, res) => {
+    const newMeeting = db.createMeeting();
+    const saved = db.addToDatabase(model, newMeeting);
+    if (saved) {
+        res.status(201).send(saved);
+    }
+});
+
+router.delete("", (req, res) => {
+    const allDeleted = db.deleteAllFromDatabase(model);
+    if (allDeleted !== null) {
+        res.status(204).send();
+    }
+});
+
+module.exports = router;

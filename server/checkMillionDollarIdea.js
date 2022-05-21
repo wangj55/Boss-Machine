a91f49b5dc2656cbf0d4e36eb2096b0d356389c6
Ji Wang <wj42421999@gmail.com>
2022-05-20 22:21:52 -0400

    add checkMillionDollarIdea to ideas

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,33 @@
-const checkMillionDollarIdea = () => {};
+const isValidIdea = (idea) => {
+    if (!idea.hasOwnProperty("numWeeks") || !idea.hasOwnProperty("weeklyRevenue")) {
+        return false;
+    }
+
+    const {numWeeks, weeklyRevenue} = idea;
+    if (isNaN(parseFloat(numWeeks)) || !isFinite(numWeeks)) {
+        return false;
+    }
+    if (isNaN(parseFloat(weeklyRevenue)) || !isFinite(weeklyRevenue)) {
+        return false;
+    }
+
+    return true;
+}
+
+const checkMillionDollarIdea = (req, res, next) => {
+    if (!isValidIdea(req.body)) {
+        res.status(400).send();
+        return;
+    }
+
+    const {numWeeks, weeklyRevenue} = req.body;
+    const value = numWeeks * weeklyRevenue;
+    if (value < 1000000) {
+        res.status(400).send();
+    } else {
+        next();
+    }
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
--- server/routes/ideas.js
+++ server/routes/ideas.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db");
+const checkMillionDollarIdea = require("../checkMillionDollarIdea");
 
 const model = "ideas";
 
@@ -22,7 +23,7 @@ router.get("/:ideaId", (req, res) => {
     res.send(db.getFromDatabaseById(model, req.ideaId));
 });
 
-router.post("", (req, res) => {
+router.post("", checkMillionDollarIdea, (req, res) => {
     const newItem = db.addToDatabase(model, req.body);
     if (newItem) {
         res.status(201).send(newItem);
@@ -31,7 +32,7 @@ router.post("", (req, res) => {
     }
 });
 
-router.put("/:ideaId", (req, res) => {
+router.put("/:ideaId", checkMillionDollarIdea, (req, res) => {
     res.send(db.updateInstanceInDatabase(model, req.body));
 });
 
